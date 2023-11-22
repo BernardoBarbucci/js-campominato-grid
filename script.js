33cce016746bb761bf5bdca5a56ea2e3d5e86555
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-22 15:54:28 +0100

    inizio struttura funzione per creare quadratini -> griglia

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 const gridEl = doument.querySelector('main .container div.main-grid');
 const startButtonEl = document.querySelector('button#start-button');
@@ -10,3 +9,13 @@ const gameModeEl = document.querySelector('select#select-difficulty');
 startButtonEl.addEventListener('click', function(){
     startNewGame(gridEl, gameModeEl)
 });
+
+// funzione dove creo i quadratini e creo una griglia
+function startNewGame(gridEl, gameModeEl) {
+
+
+}
+
+
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto 
+    // un messaggio in console con il numero della cella cliccata.
